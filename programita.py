df4d74393ae59efd81daf35ad51350171166c8f6
aaronbblanco <aaronblanmeis@gmail.com>
2021-03-09 01:31:24 +0100

    segundo commit

--- programita.py
+++ programita.py
@@ -8,11 +8,18 @@ def sumar(num1 , num2):
 def multiplicar(num1 , num2):
     return num1 * num2
 
+def division(num1 , num2):
+    return num1/num2
+
 def calculadora(num1 , num2):
     if(funcion == 'sumar'):
         sumar(num1 , num2)
-    else:
+    elif(funcion == 'division'):
+        division(num1 , num2)
+    elif(funcion == 'multiplicar'):
         multiplicar(num1,num2)
+    else:
+        'error'
 
 num1 = input()
 num2 = input()
